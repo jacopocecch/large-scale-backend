848311e186e9bfbc01af4501eef9db81b49ffdc2
Jacopo Cecchetti <j.cecchetti23@gmail.com>
2022-01-22 00:24:28 +0100

    Fixed Comment Repository Bean

--- src/main/java/com/unipi/data/mining/backend/repositories/CustomCommentRepository.java
+++ src/main/java/com/unipi/data/mining/backend/repositories/CustomCommentRepository.java
@@ -1,10 +1,12 @@
 package com.unipi.data.mining.backend.repositories;
 
 import com.unipi.data.mining.backend.entities.mongodb.Comment;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
 
+@Repository
 public class CustomCommentRepository extends CustomRepository{
 
     public List<Comment> bulkInsertComments(List<Comment> comments) {
